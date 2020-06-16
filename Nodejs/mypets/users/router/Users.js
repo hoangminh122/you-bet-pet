df984712e7a7fa4e655b7877f5987fcd8b903190
hoangminh122 <hoangminh12297@gmail.com>
2020-06-16 21:59:34 +0700

    del

--- Nodejs/mypets/users/router/Users.js
+++
@@ -1,7 +0,0 @@
-const {createUserController,userLoginController} = require("../controller/Users")
-const router = require("express").Router();
-
-router.post("/createUser",createUserController);
-router.post("/login",userLoginController);
-
-module.exports = router;
