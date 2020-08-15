3bd65615418e7bdce7db140a5d40ff2b24e8eb9a
unknown <hoangminh12297@gmail.com>
2020-08-15 15:40:12 +0700

    update clean code

--- ReactNative/MyPet/src/components/login/LoginEmail.js
+++ ReactNative/MyPet/src/components/login/LoginEmail.js
@@ -1,48 +1,44 @@
 import React, { Component } from 'react';
-import {View,Text,Image, TextInput, Dimensions, TouchableOpacity, Alert,StyleSheet,KeyboardAvoidingView} from 'react-native'
+import { View, Text, Image, TextInput, Dimensions, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native'
 import firebaseConfig from '../../config/ConfigFirebase'
 import firebase from 'firebase'
 import axios from 'axios'
 import Header from '../header_footer/header'
 
+let screen = Dimensions.get('window');
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-let screen = Dimensions.get('window');
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
 export default class loginEmail extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            email : '',
-            password : '',
-            sucsess:-1,
-            username:'',
-            avatar:''
+            email: '',
+            password: '',
+            sucsess: -1,
+            username: '',
+            avatar: ''
         }
     }
 
-     getInforUser = async() => {
-        axios.get('http://192.168.1.3:3006/user/'+this.state.email,{
-            email:this.state.email,
-            password:this.state.password
-            })
-        .then(response => {
-            console.log(response.data)
-        // this.props.history.push('/inforAuction');
+    getInforUser = async () => {
+        axios.get('http://192.168.1.3:3006/user/' + this.state.email, {
+            email: this.state.email,
+            password: this.state.password
         })
-        .catch(error => {
-        console.log(error);
-        });
+            .then(response => {
+                console.log(response.data)
+                // this.props.history.push('/inforAuction');
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     loginEmail = () => {
-        
+
         // firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
         // .then(()=>{
         //     Alert.alert("Dang nhap thanh cong");
@@ -55,10 +51,10 @@ export default class loginEmail extends Component {
         //     // ...
         //   });
 
-        axios.post('http://192.168.1.3:3006/auth/login',{
-                email:this.state.email,
-                password:this.state.password
-                })
+        axios.post('http://192.168.1.3:3006/auth/login', {
+            email: this.state.email,
+            password: this.state.password
+        })
             .then(response => {
                 console.log(response.data)
                 this.getInforUser();
@@ -68,59 +64,111 @@ export default class loginEmail extends Component {
                 //     username: userFace.user.displayName,
                 //     email: userFace.user.email,
                 //     avatar : userFace.user.photoURL
-                  
+
                 //   });
                 //   //end
 
 
-            // this.setState({ sucsess:1 });
-            // this.props.history.push('/inforAuction');
+                // this.setState({ sucsess:1 });
+                // this.props.history.push('/inforAuction');
             })
             .catch(error => {
-            console.log(error);
+                console.log(error);
             });
     }
-  render() {
-    return (
-      
-        <View style={styles.container}  >
-             <Header nameTitle = 'Quay Lại' goBack = {this.goBack}/>
-             <KeyboardAvoidingView style={{flex:1.5}}
-            >
-             <View style={{flex:1,width:'100%',backgroundColor:''}}>
-                <Image style={{marginLeft:20,marginTop:0,width:150,height:100,color:'red'}} source={require('../../images/logo.png')}></Image>
-             </View>
-            </KeyboardAvoidingView>
 
-             <View style={{flex:8,width:'100%'}}>
-                <Text style={{fontSize:25,fontWeight:'100',marginLeft:20,marginBottom:10,marginTop:20,color:'black'}}>Wellcome,</Text>
-                <Text style={{fontSize:15,fontWeight:'100',marginLeft:20,marginBottom:20,color:'gray'}}>Sign in to access your account</Text>
-                <TextInput style={{height:screen.height/16,alignSelf:'stretch',borderRadius:5,borderBottomWidth:1,borderColor:'gray',margin:5,marginHorizontal:20}} placeholder="Email" onChangeText={ (events) =>
-                    this.setState({email:events})
-                }   value={this.state.email}/>
-                <TextInput type="password" style={{height:screen.height/16,alignSelf:'stretch',borderRadius:5,borderBottomWidth:1,borderColor:'gray',margin:5,marginHorizontal:20}} placeholder="Password" onChangeText ={ (events) =>
-                    this.setState({password:events})
-                }  value={this.state.password}/>
-        
-                <TouchableOpacity style={{marginTop:10,borderColor:'gray',borderRadius:1,borderWidth:1,backgroundColor:'red',padding:10,marginHorizontal:20}} onPress={()=> this.loginEmail()}>
-                    <Text style={{textAlign:'center',color:'white'}}>
-                        Login
-                    </Text>
-                </TouchableOpacity>
-             </View>
-           
-       
-        </View>
+    goBack = () => {
+        this.props.history.goBack();
+    }
 
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}  >
+                <Header nameTitle='Quay Lại' goBack={this.goBack} />
+                <KeyboardAvoidingView style={{ flex: 1.5 }}
+                >
+                    <View style={{ flex: 1, width: '100%', backgroundColor: '' }}>
+                        <Image style={styles.imgLogo} source={require('../../images/logo.png')}></Image>
+                    </View>
+                </KeyboardAvoidingView>
+                <View style={styles.body}>
+                    <Text style={styles.txtTitle}>Wellcome,</Text>
+                    <Text style={[styles.txtTitle,{ fontSize: 15, marginBottom: 20, color: 'gray',marginTop:0 }]}>Sign in to access your account</Text>
+                    <TextInput style={styles.txtInput} placeholder="Email" 
+                        onChangeText={(events) =>
+                            this.setState({ email: events })
+                        } 
+                        value={this.state.email} 
+                    />
+                    <TextInput secureTextEntry={true} type="password" 
+                        style={styles.txtInput} 
+                        placeholder="Password" 
+                        onChangeText={(events) =>
+                            this.setState({ password: events })
+                        } 
+                        value={this.state.password} 
+                    />
+                    <TouchableOpacity 
+                        style={styles.btnLogin}
+                         onPress={() => this.loginEmail()}
+                    >
+                        <Text style={styles.txtLogin}>
+                            Login
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-    container:{
-      display:'flex',
-      flexDirection:'column',
-      flex:1,
-      backgroundColor:'#F8F8FF'
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        backgroundColor: '#F8F8FF'
     },
+    imgLogo:{
+        marginLeft: 20, 
+        marginTop: 0, 
+        width: 150, 
+        height: 100, 
+        color: 'red'
+    },
+    body:{
+        flex: 8,
+        width: '100%'
+    },
+        txtTitle:{
+            fontSize: 25, 
+            fontWeight: '100', 
+            marginLeft: 20,
+            marginBottom: 10, 
+            marginTop: 20, 
+            color: 'black'
+        },
+        txtInput:{
+            height: screen.height / 16, 
+            alignSelf: 'stretch', 
+            borderRadius: 5, 
+            borderBottomWidth: 1, 
+            borderColor: 'gray', 
+            margin: 5,
+            marginHorizontal: 20
+        },
+        btnLogin:{
+            marginTop: 10, 
+            borderColor: 'gray', 
+            borderRadius: 1, 
+            borderWidth: 1, 
+            backgroundColor: 'red', 
+            padding: 10, 
+            marginHorizontal: 20
+        },
+            txtLogin:{
+                textAlign: 'center', 
+                color: 'white'
+            }
 })
--- ReactNative/MyPet/src/components/payment/Payment.js
+++ ReactNative/MyPet/src/components/payment/Payment.js
@@ -29,60 +29,61 @@ export default class CreateSession extends Component {
          <View style={styles.container}>
             <Header nameTitle = "Thanh Toan"/>
             <View style={styles.body}>
-            <View style = {{ flex:2}}>
-                <FlatList
-                    style={{height:screen.height,width:screen.width,backgroundColor:'white'}}
-                    data={this.state.dataSource}
-                    renderItem={({item,index}) =>  
-                    <View style={[styles.bodyTop10Object,{height:screen.height/8}]} key={{index}}>
-                        <View style={[styles.bodyTop10ObjectStt]}>
-                        <Text>1</Text>
-                        </View>
-                        <View style={styles.bodyTop10ObjectImage}>
-                        <Image style={styles.bodyTop10ObjectImageChild}></Image>
-                        </View>
-                        <View style={styles.bodyTop10ObjectInfor}>
-                        <View>
-                            <Text style={styles.bodyTop10ObjectInforTxt}>Pet-362</Text>
-                        </View>
-                        <View>
-                            <Text style={styles.bodyTop10ObjectInforTxt}>582.500.00 đ</Text>
-                        </View>
-                        </View>
+              <View style = {{ flex:2}}>
+                  <FlatList
+                      style={{height:screen.height,width:screen.width,backgroundColor:'white'}}
+                      data={this.state.dataSource}
+                      renderItem={({item,index}) =>  
+                      <View style={[styles.bodyTop10Object,{height:screen.height/8}]} key={{index}}>
+                          <View style={[styles.bodyTop10ObjectStt]}>
+                          <Text>1</Text>
+                          </View>
+                          <View style={styles.bodyTop10ObjectImage}>
+                          <Image style={styles.bodyTop10ObjectImageChild}></Image>
+                          </View>
+                          <View style={styles.bodyTop10ObjectInfor}>
+                          <View>
+                              <Text style={styles.bodyTop10ObjectInforTxt}>Pet-362</Text>
+                          </View>
+                          <View>
+                              <Text style={styles.bodyTop10ObjectInforTxt}>582.500.00 đ</Text>
+                          </View>
+                          </View>
+                      </View>
+                      } 
+                      keyExtractor={(item, index) => index.toString()}
+                  />
+              </View>
+              <View style = {styles.bodyMoneyCount}>
+                <View style={styles.bodyMoneyCountChild}>
+                  <View style={{flex:1,}}>
+                    <View style={styles.moneyTemp}>
+                      <Text style={{flex:1}}>Tạm tính </Text>
+                      <Text style={{flex:1}}>1165.000.00 đ</Text>
                     </View>
-                    } 
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-            <View style = {{ flex:1,flexDirection:'column',justifyContent:'flex-end',alignItems:'flex-end'}}>
-              <View style={{flex:0.5,flexDirection:'column',width:screen.width/2}}>
-                <View style={{flex:1,}}>
-                  <View style={{flex:1,flexDirection:'row'}}>
-                    <Text style={{flex:1}}>Tạm tính </Text>
-                    <Text style={{flex:1}}>1165.000.00 đ</Text>
+                  </View>
+                  <View style={styles.moneyVat}>
+                    <Text  style={{flex:1}}>Phụ phí</Text>
+                    <Text  style={{flex:1}}> 3.250.00đ</Text>
+                  </View>
+                  <View style={styles.moneyResult}>
+                    <Text style={{flex:1}} >Tổng cộng</Text>
+                    <Text  style={styles.txtMoneyResult}>1168.250.00đ</Text>
                   </View>
                 </View>
-                <View style={{flex:1,flexDirection:'row'}}>
-                  <Text  style={{flex:1}}>Phụ phí</Text>
-                  <Text  style={{flex:1}}> 3.250.00đ</Text>
-                </View>
-                <View style={{flex:1,flexDirection:'row'}}>
-                  <Text style={{flex:1}} >Tổng cộng</Text>
-                  <Text  style={{flex:1,color:'red',fontWeight:'bold'}}>1168.250.00đ</Text>
-                </View>
               </View>
-            </View>
-
-            <TouchableOpacity style = {{ flex:0.5,flexDirection:'row',justifyContent:'center',alignItems:'center',borderRadius:10,margin:10,backgroundColor:'red'}}>
-              <View style={{flex:0.5,justifyContent:'center',alignItems:'center'}}>
-                    <Text style={{color:'white',fontWeight:'normal',fontSize:15}}>Thanh Toán</Text>
-              </View>
-            </TouchableOpacity>
+              <TouchableOpacity style = {styles.btnPayment}>
+                <View style={styles.viewPayment}>
+                      <Text style={styles.txtPayment}>Thanh Toán</Text>
+                </View>
+              </TouchableOpacity>
                 
             </View>
             <Footer/>
         </View>
       </DismissKeyboard>
+      
+
       
     );
   }
@@ -94,32 +95,11 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor:'#F8F8FF'
   },
-  imagePets:{
-    flex:3,
-    // backgroundColor:'#F8F8FF',
-    justifyContent:'center',
-    alignItems:'center'
-  },
-    imageChild:{
-      height:'90%',
-      width:'90%',
-      backgroundColor:'#ABCDFF'
-    },
   body:{
     flex:6,
     backgroundColor:'#F8F8FF',
     flexDirection:'column'
   },
-    bodyTittle:{
-      flex:1.5,
-      backgroundColor:'#F8F8FF'
-    },
-      bodyTittleTxt:{
-        marginLeft:10,
-        fontSize:20,
-        fontWeight:'bold',
-        color:'#000000'
-      },
     bodyTop10:{
       flex:8.5,
       backgroundColor:'#F8F8FF',
@@ -163,7 +143,55 @@ const styles = StyleSheet.create({
         bodyTop10ObjectInforTxt:{
           fontWeight:'bold',
           fontSize:12
+        },
+    bodyMoneyCount:{
+      flex:1,
+      flexDirection:'column',
+      justifyContent:'flex-end',
+      alignItems:'flex-end'
+    },
+      bodyMoneyCountChild:{
+        flex:0.5,
+        flexDirection:'column',
+        width:screen.width/2
+      },
+        moneyTemp:{
+          flex:1,
+          flexDirection:'row'
+        },
+        moneyVat:{
+          flex:1,
+          flexDirection:'row'
+        },
+        moneyResult:{
+          flex:1,
+          flexDirection:'row'
+        },
+          txtMoneyResult:{
+            flex:1,
+            color:'red',
+            fontWeight:'bold'
+          },
+    btnPayment:{
+      flex:0.5,
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:10,
+      margin:10,
+      backgroundColor:'red'
+    },
+      viewPayment:{
+        flex:0.5,
+        justifyContent:'center',
+        alignItems:'center'
+      },
+        txtPayment:{
+          color:'white',
+          fontWeight:'normal',
+          fontSize:15
         }
+
  });
 //  function mapStateToProps(state){
 //   return {
